a3ed8b9ff888e9d644f760d6874dd088438528f9
Timothy Yang <timothyoung97@gmail.com>
2022-06-01 12:11:57 +0800

    Add in profile pic & restyle headline

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import {Container, Box, Heading} from '@chakra-ui/react'
+import {Container, Box, Heading, Image, useColorModeValue} from '@chakra-ui/react'
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
                 Hello, I&apos;m Timothy Yang, a computer science student at the University of Singapore.
             </Box>
             <Box display={{md:'flex'}}>
@@ -11,8 +11,26 @@ const Page = () => {
                     <Heading as="h2" variant="page-title">
                         Timothy Yang
                     </Heading>
-                    
                     <p>Aspiring Software Developer</p>
+                </Box>
+
+                <Box
+                    flexShrink={0}
+                    mt={{ base: 4, md: 0 }}
+                    ml={{ md: 6 }}
+                    textAlign="center"
+                >
+                    <Image 
+                        borderColor="whiteAlpha.800"
+                        borderWidth={2}
+                        borderStyle="solid"
+                        w="100px"
+                        h="100px"
+                        display="inline-block"
+                        borderRadius="full"
+                        src="images/timothy_square.jpg"
+                        alt="Profile Picture"
+                    />
                 </Box>
             </Box>
         </Container>
