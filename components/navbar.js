395ae9466458fe554cde2036cc539fa958a680b5
Timothy Yang <timothyoung97@gmail.com>
2022-06-01 11:47:03 +0800

    Refactor & resolve alignment error

--- components/navbar.js
+++ components/navbar.js
@@ -18,15 +18,17 @@ import {
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button.js'
 
-const LinkItem = ({ href, path, children }) => {
+const LinkItem = ({ href, path, target, children, ...props }) => {
     const active = path === href
     const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
     return (
         <NextLink href={href}>
             <Link
                 p={2}
-                bg={active ? 'glassTeal' : undefined}
+                bg={active ? 'grassTeal' : undefined}
                 color={active ? '#202023' : inactiveColor}
+                target={target}
+                {...props}
             >
                 {children}
             </Link>
@@ -43,16 +45,16 @@ const Navbar = props => {
             as="nav"
             w="100%"
             bg={useColorModeValue('#ffffff40', '#20202380')}
-            css={{backdropFilter:'blur(10px)'}}
+            css={{ backdropFilter: 'blur(10px)' }}
             zIndex={1}
             {...props}
         >
             <Container 
-                display="flex" 
-                p={2} 
-                maxW="container.md" 
-                wrap="wrap" 
-                align="center" 
+                display="flex"
+                p={2}
+                maxW="container.md"
+                wrap="wrap"
+                align="center"
                 justify="space-between"
             >
                 <Flex align="center" mr={5}>
@@ -61,12 +63,12 @@ const Navbar = props => {
                     </Heading>
                 </Flex>
                 <Stack
-                    direction={{base:'column', md:'row'}}
-                    display={{base:'none', md:'flex'}}
-                    width={{base: 'full', md: 'auto'}}
+                    direction={{ base: 'column', md: 'row' }}
+                    display={{ base: 'none', md: 'flex' }}
+                    width={{ base: 'full', md: 'auto' }}
                     alignItems="center"
                     flexGrow={1}
-                    mt={{base: 4, md: 0}}
+                    mt={{ base: 4, md: 0 }}
                 >
                     <LinkItem href="/works" path={path}>
                         Works
@@ -76,11 +78,16 @@ const Navbar = props => {
                     </LinkItem>
                 </Stack>
 
-                <Box flex={1} aligh="right">
+                <Box flex={1} align="right">
                     <ThemeToggleButton />
                     <Box ml={2} display={{base: 'inline-block', md:'none'}}>
-                        <Menu>
-                            <MenuButton as={IconButton} icon={<HamburgerIcon/>} variant="outline" aria-label="Options" />
+                        <Menu isLazy id="navbar-menu">
+                            <MenuButton
+                                as={IconButton}
+                                icon={<HamburgerIcon />}
+                                variant="outline"
+                                aria-label="Options"
+                            />
                         <MenuList>
                             <NextLink href="/" passHref>
                                 <MenuItem as={Link}>
