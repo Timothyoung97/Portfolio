c3f7ebd241f593c7edc7b2966acfff8eb965a4ff
Timothy Yang <timothyoung97@gmail.com>
2022-07-18 22:57:25 +0800

    Add favicon

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -16,6 +16,13 @@ const Main = ({ children, router}) => {
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <title>Timothy Yang - Homepage</title>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#000000" />
+                <meta name="theme-color" content="#000000"></meta>
             </Head>
 
             <Navbar path={router.asPath} />
